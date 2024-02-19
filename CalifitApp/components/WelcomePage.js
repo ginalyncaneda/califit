6d239f2db316d193e1ac09042fbd3a2075b86138
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-20 02:08:14 +0800

    Add content and styles in Welcome Page

--- CalifitApp/components/WelcomePage.js
+++ CalifitApp/components/WelcomePage.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const WelcomePage = () => {
+  const handleRegistration = () => {
+    // Handle navigation or any other action when the button is pressed
+    console.log('Start Your Journey');
+  };
+
   return (
-    <View>
-      <Text>This is the Additional Component!</Text>
+    <View style={styles.container}>
+      <Text style={[styles.text, { fontSize: 24 }]}>Welcome!</Text>
+      <Text style={[styles.text, { fontSize: 16 }]}>CaliFit welcomes you to a world of calisthenics innovation, 
+                                where every rep brings you closer to your best self.
+      </Text>
+      <TouchableOpacity style={styles.button} onPress={handleRegistration}>
+        <Text style={styles.buttonText}>Start Your Journey</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: '#820000',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#820000',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
 export default WelcomePage;
