4464b44e816f3d427e85dabc1bd18dfe55528dd9
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-20 01:48:46 +0800

    Add Welcome Page

---
+++ CalifitApp/components/WelcomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WelcomePage = () => {
+  return (
+    <View>
+      <Text>This is the Additional Component!</Text>
+    </View>
+  );
+};
+
+export default WelcomePage;
