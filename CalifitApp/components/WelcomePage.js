3f8a0eae16760d61ad64d91c8ebae798f6c447de
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-20 02:16:25 +0800

    Import welcome image

--- CalifitApp/components/WelcomePage.js
+++ CalifitApp/components/WelcomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import welcomepic from '../images/welcomepic.png';
 
 const WelcomePage = () => {
   const handleRegistration = () => {
@@ -9,6 +10,7 @@ const WelcomePage = () => {
 
   return (
     <View style={styles.container}>
+      <Image source={welcomepic} style={styles.image} />
       <Text style={[styles.text, { fontSize: 24 }]}>Welcome!</Text>
       <Text style={[styles.text, { fontSize: 16 }]}>CaliFit welcomes you to a world of calisthenics innovation, 
                                 where every rep brings you closer to your best self.
@@ -25,6 +27,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
   },
   text: {
     fontSize: 20,
