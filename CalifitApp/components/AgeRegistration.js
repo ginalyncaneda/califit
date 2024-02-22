0af7ac6c08b5ff0a6204f1905836e5d4ff2f47b0
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-22 21:15:19 +0800

    Create Age registration page

---
+++ CalifitApp/components/AgeRegistration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AgeRegistration = () => {
+    return (
+        <View>
+            <Text>Age Registration</Text>
+        </View>
+    );
+};
+
+export default AgeRegistration;
