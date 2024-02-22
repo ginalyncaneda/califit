04590c2e8815b09f23091fa42bee802d7f73796e
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-22 20:25:58 +0800

    Import namepic

--- CalifitApp/components/NameRegistration.js
+++ CalifitApp/components/NameRegistration.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import namepic from '../images/name.png';
+
 const NameRegistration = () => {
 
   return (
     <View style={styles.container}>
         <Text>Name</Text>
+        <Image source={namepic} style={styles.image} />
     </View>
   );
 };
