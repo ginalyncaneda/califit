45feb336a6cbf0b685b4a970cfd303c76a5cf4a9
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-22 21:10:58 +0800

    Add input ad button

--- CalifitApp/components/NameRegistration.js
+++ CalifitApp/components/NameRegistration.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 import namepic from '../images/name.png';
 
 const NameRegistration = () => {
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
 
   return (
     <View style={styles.container}>
-        <Text>What is your name?</Text>
+    <View style={styles.formcontainer}>
+        <Text style={styles.header}>Sign Up</Text>
+        <Text style={styles.title}>What is your name?</Text>
         <Image source={namepic} style={styles.image} />
+        <TextInput
+              style={styles.input}
+              placeholder="First Name"
+              value={firstname}
+              onChangeText={setFirstname}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Last Name"
+              value={lastname}
+              onChangeText={setLastname}
+            />   
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Next</Text>
+            </TouchableOpacity>
+    </View>
     </View>
   );
 };
@@ -19,15 +39,55 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: '#820000',
     },
+    formContainer: {
+      flex: 1,
+      width: '100%',
+      maxWidth: 400,
+      alignSelf: 'center',
+      backgroundColor: '#820000',
+      borderRadius: 10,
+      padding: 20,
+    },
+    header: {
+      fontSize: 12,
+      fontWeight: 'bold',
+      marginBottom: 10,
+      marginLeft: 20,
+      textAlign: 'center',
+      color: '#ffffff',
+    },
+    title: {
+      fontSize: 45,
+      fontWeight: 'semibold',
+      marginBottom: 10,
+      marginTop: 20,
+      marginLeft: 20,
+      textAlign: 'center',
+      color: '#ffffff',
+    },
     image: {
-      width: 200,
-      height: 200,
+      width: 250,
+      height: 250,
       marginBottom: 20,
+      alignSelf: 'center',
     },
     text: {
       fontSize: 20,
       marginBottom: 20,
-      color: '#ffffff',
+      color: 'white',
+      textAlign: 'center',
+    },
+    input: {
+      height: 40,
+      width: 250,
+      marginVertical: 10,
+      borderWidth: 1.5,
+      borderColor: '#ccc',
+      borderRadius: 50,
+      padding: 10,
+      backgroundColor: '#fff',
+      marginBottom: 10,
+      alignSelf: 'center',
       textAlign: 'center',
     },
     button: {
@@ -35,12 +95,15 @@ const styles = StyleSheet.create({
       paddingHorizontal: 30,
       paddingVertical: 15,
       borderRadius: 50,
+      width: 100,
+      alignSelf: 'flex-end',
+      marginTop: 50,
     },
     buttonText: {
       color: 'white',
       fontWeight: 'bold',
+      textAlign: 'center',
     },
   });
-
 
 export default NameRegistration;
