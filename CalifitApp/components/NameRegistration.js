c395e33bafd0be74338b2d326a7620c2aacd98cd
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-22 20:23:33 +0800

    Create Name Registration Page

---
+++ CalifitApp/components/NameRegistration.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+const NameRegistration = () => {
+
+  return (
+    <View style={styles.container}>
+        <Text>Name</Text>
+    </View>
+  );
+};
+
+
+export default NameRegistration;
