e8a98a4d49274c70d6f251993d6d7e51cf1b3a95
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-22 20:29:14 +0800

    Add styles

--- CalifitApp/components/NameRegistration.js
+++ CalifitApp/components/NameRegistration.js
@@ -6,11 +6,41 @@ const NameRegistration = () => {
 
   return (
     <View style={styles.container}>
-        <Text>Name</Text>
+        <Text>What is your name?</Text>
         <Image source={namepic} style={styles.image} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#820000',
+    },
+    image: {
+      width: 200,
+      height: 200,
+      marginBottom: 20,
+    },
+    text: {
+      fontSize: 20,
+      marginBottom: 20,
+      color: '#ffffff',
+      textAlign: 'center',
+    },
+    button: {
+      backgroundColor: 'black',
+      paddingHorizontal: 30,
+      paddingVertical: 15,
+      borderRadius: 50,
+    },
+    buttonText: {
+      color: 'white',
+      fontWeight: 'bold',
+    },
+  });
 
 
 export default NameRegistration;
