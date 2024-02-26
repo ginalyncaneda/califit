c06f6e2694021f1f157d0910f7bdb6fabebf8477
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-02-26 14:11:46 +0800

    Create Gender Registration Page

---
+++ CalifitApp/components/GenderRegistration.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import  { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+
+const GenderRegistration = () => {
+
+    return (
+        <View >
+        
+            <Text >Sign Up</Text>
+            <Text >What is your gender?</Text>
+            
+        </View>
+    );
+};
+
+
+
+
+export default GenderRegistration;
