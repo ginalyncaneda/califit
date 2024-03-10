4d9e2afd8ee56325d17c624ac3a0ea4abfef0453
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-03-10 21:20:27 +0800

    Create Exercise Selection Page

---
+++ CalifitApp/components/ExerciseSelection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+
+const ExerciseSelection = () => {
+  return (
+    <View>
+      
+    </View>
+  );
+
+  }
+
+export default ExerciseSelection;
