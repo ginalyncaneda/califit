573c5298177bb62741691bc579acc8f6d3bffc6c
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2024-03-10 23:52:13 +0800

    Fix error

--- CalifitApp/components/UserRegistration.js
+++ CalifitApp/components/UserRegistration.js
@@ -18,10 +18,6 @@ export default function UserRegistration() {
       {showLiveVideoCapture ? (
         <LiveVideoCapture />
       ) : (
-        <View style={styles.container}>
-      {showLiveVideoCapture ? (
-        <LiveVideoCapture />
-      ) : (
         <View>
           <View style={styles.formContainer}>
           <Image source={applogo} style={styles.image} />
