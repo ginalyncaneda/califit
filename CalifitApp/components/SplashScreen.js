3254be6898bf725ffb6cb27bc3b6f9f1f704a0f1
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-03-11 21:29:18 +0800

    Add styles and navigation

--- CalifitApp/components/SplashScreen.js
+++ CalifitApp/components/SplashScreen.js
@@ -1,46 +1,52 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Image, Text, StyleSheet, Animated, Easing } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, Image, Animated, Easing, StyleSheet } from 'react-native';
 import applogo from '../images/califit-logo.png';
 
-const SplashScreen = () => {
-  const [loading, setLoading] = useState(true);
+const SplashScreen = ({ navigation }) => {
   const flipValue = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
+      navigation.replace('WelcomePage');
+    }, 3000); // Adjust the timing if needed
+
+    Animated.timing(flipValue, {
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [navigation]);
 
   const splashScreenStyle = {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'transparent',
+    backgroundColor: '#F9E79F',
   };
 
   const flip = flipValue.interpolate({
     inputRange: [0, 0.5, 1],
-    outputRange: [1, -1, 1],
+    outputRange: ['0deg', '180deg', '360deg'],
   });
-
-  Animated.timing(flipValue, {
-    toValue: 1,
-    duration: 2000,
-    easing: Easing.linear,
-    useNativeDriver: true,
-  }).start();
 
   return (
     <View style={splashScreenStyle}>
       <Animated.Image
         source={applogo}
-        style={{ width: 230, height: 230, transform: [{ scaleX: flip }] }}
+        style={[styles.logo, { transform: [{ rotateY: flip }] }]}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 230,
+    height: 230,
+  },
+});
 
 export default SplashScreen;
