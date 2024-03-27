5a4cd39d79abd4a2cac768deaee0da88215bb6de
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-03-28 00:41:05 +0800

    Add Stack in Tabs

--- CalifitApp/components/Tabs.js
+++ CalifitApp/components/Tabs.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Dashboard from './Dashboard';
 import TutorialScreen from './TutorialScreen';
 import ChallengesScreen from './ChallengeScreen';
 import ProfileScreen from './ProfileScreen';
+import EditProfile from './EditProfile'; // Make sure to import EditProfile
 
 const Tab = createBottomTabNavigator();
+const ProfileStack = createStackNavigator();
+
+// Define the Profile stack that includes ProfileScreen and EditProfile
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator screenOptions={{ headerShown: false }}>
+      <ProfileStack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <ProfileStack.Screen name="EditProfile" component={EditProfile} />
+    </ProfileStack.Navigator>
+  );
+}
 
 export default function Tabs() {
   return (
     <Tab.Navigator
-    screenOptions={({ route }) => ({
-      headerShown: false,
+      screenOptions={({ route }) => ({
+        headerShown: false,
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
-
           if (route.name === 'Home') {
             iconName = 'home';
           } else if (route.name === 'Tutorial') {
@@ -25,8 +37,6 @@ export default function Tabs() {
           } else if (route.name === 'Profile') {
             iconName = 'user';
           }
-
-          // You can return any component that you like here!
           return <FontAwesome5 name={iconName} size={size} color={color} solid={focused} />;
         },
         tabBarActiveTintColor: '#820000',
@@ -36,7 +46,7 @@ export default function Tabs() {
       <Tab.Screen name="Home" component={Dashboard} />
       <Tab.Screen name="Tutorial" component={TutorialScreen} />
       <Tab.Screen name="Challenges" component={ChallengesScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Profile" component={ProfileStackScreen} />
     </Tab.Navigator>
   );
 }
