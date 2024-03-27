bbb90bfe677d17812cb72c8665e49e5222eb0027
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-03-27 12:04:44 +0800

    Add routing for tabs

--- CalifitApp/App.js
+++ CalifitApp/App.js
@@ -14,6 +14,7 @@ import ExerciseSelection from './components/ExerciseSelection';
 import LiveVideoCapture from './components/LiveVideoCapture';
 import LiveVideo from './components/LiveVideo';
 import Dashboard from './components/Dashboard';
+import Tabs from './components/Tabs';
 
 const Stack = createNativeStackNavigator();
 
@@ -31,7 +32,7 @@ export default function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="ExerciseSelection" component={ExerciseSelection} />
         <Stack.Screen name="LiveVideo" component={LiveVideo} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="MainApp" component={Tabs} options={{ headerShown: false }} />
         {/* Add other screens as needed */}
       </Stack.Navigator>
 
